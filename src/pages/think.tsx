920876ba263390e207d339d00fcdb379cd852153
takashi0602 <t.takashi0602@gmail.com>
2023-08-19 02:37:18 +0900

    feat: add structure

--- src/pages/think.tsx
+++ src/pages/think.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Button } from "../components/button";
 
 const Think: React.FC = () => {
   return (
     <div>
-      <h1>Think Page</h1>
+      <h1>考え中</h1>
+      <div>
+        <span>歩数: </span>100<span>歩</span>
+      </div>
+      <p>
+        Q.
+        問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い
+      </p>
+      <Button>カンニングする！</Button>
+      <Button>答え出た！</Button>
     </div>
   );
 };
