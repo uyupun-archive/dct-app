0d642a83e5be71b1ccea38edd73e5fe6e2e8d919
ウヰスキー響 <yamazaki@uwisukikyounoMacBook-Air.local>
2023-08-19 07:59:43 +0900

    feat: edit tsx & scss

--- src/pages/Top/index.tsx
+++ src/pages/Top/index.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import styles from "./styles.module.scss"
+import { Button } from "../../components/Button";
 
 const Top: React.FC = () => {
   return (
-    <div>
-      <h1>Don’t cunning! Think.</h1>
+    <div className={styles.container}>
+      <div>
+        <h1>Don’t cunning! <div className={styles["font-bold"]}>Think.</div></h1>
+
+        <div className={ styles[ "button-area" ] }>
+          <Button
+            type="button"
+            onClick={ () => {
+            }}>
+              ウォレットを接続
+          </Button>
+        </div>
+
+        <div className={ styles[ "button-area" ] }>
+          <Button
+            type="button"
+            onClick={ () => {
+            }}>
+              考える！
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
