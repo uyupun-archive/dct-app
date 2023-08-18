5dbf895df0430201332aa708433aab9b1ba35428
takashi0602 <t.takashi0602@gmail.com>
2023-08-19 02:45:38 +0900

    feat: create global.scss

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
 import "modern-css-reset/dist/reset.min.css";
+import "./styles/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
