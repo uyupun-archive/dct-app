4bec0f428c7f099b8a895c9ca267fd3109b7e749
ウヰスキー響 <yamazaki@uwisukikyounoMacBook-Air.local>
2023-08-19 12:34:38 +0900

    feat: edit tsx & scss

--- src/pages/Complete/index.tsx
+++ src/pages/Complete/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styles from "./styles.module.scss"
+import { Button } from "../../components/Button";
 
 const Complete: React.FC = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>お疲れ様でした</h1>
+      <div className={styles["button-area"]}>
+        <Button type="button" onClick={() => {}}>
+          トップへ戻る！
+          </Button>
+          </div>
     </div>
   );
 };
