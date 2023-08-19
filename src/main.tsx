4c1891e6134daa6bb637b943723005b81a8d1587
takashi0602 <t.takashi0602@gmail.com>
2023-08-19 17:51:52 +0900

    feat: create tepCountAtom and add jotai provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "jotai";
 import { router } from "./router.tsx";
 import "modern-css-reset/dist/reset.min.css";
 import "./styles/global.scss";
@@ -10,8 +11,10 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <Provider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
