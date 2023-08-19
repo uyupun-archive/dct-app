b196be8837343fa47d0b951d0f9168c0de69f239
ウヰスキー響 <yamazaki@uwisukikyounoMacBook-Air.local>
2023-08-19 18:00:48 +0900

    code fix

--- src/pages/Answer/index.tsx
+++ src/pages/Answer/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./styles.module.scss"
-import { LinkButton } from "../../components/LinkButton";
+import { Button } from "../../components/Button";
 
 const Answer: React.FC = () => {
   return (
-    <div className={styles.container}>
+    <form onSubmit={ () => {} } className={styles.container}>
       <div>
         <h1 className={styles.heading}>まとめ</h1>
         
@@ -13,13 +13,13 @@ const Answer: React.FC = () => {
           問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い
         </p>
 
-        <p className={styles.answer}>あなたの考え</p>
-        <textarea className={ styles["text-area"] } />
+        <label htmlFor="answer" className={styles.answer}>あなたの考え</label>
+        <textarea id="answer" name="answer" className={ styles["text-area"] } />
       </div>
       <div className={styles["button-area"]}>
-        <LinkButton href="/complete">答える！</LinkButton>
+        <Button type="submit">答える！</Button>
       </div>
-    </div>
+    </form>
   );
 };
 
