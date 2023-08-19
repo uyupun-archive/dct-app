74fa6d4290ba9203eacdd37aa4cb48d8d03c0b9b
takashi0602 <t.takashi0602@gmail.com>
2023-08-19 16:57:46 +0900

    feat: add QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./router.tsx";
 import "modern-css-reset/dist/reset.min.css";
 import "./styles/global.scss";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
