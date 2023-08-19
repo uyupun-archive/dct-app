deb8b35b6c5e338b8440efd31d714b1cff0f9598
ウヰスキー響 <yamazaki@uwisukikyounoMacBook-Air.local>
2023-08-19 15:07:33 +0900

    feat: edit tsx & scss

--- src/pages/Answer/index.tsx
+++ src/pages/Answer/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import styles from "./styles.module.scss"
+import { LinkButton } from "../../components/LinkButton";
 
 const Answer: React.FC = () => {
   return (
-    <div>
-      <h1>まとめ</h1>
+    <div className={styles.container}>
+      <div>
+        <h1 className={styles.heading}>まとめ</h1>
+        
+        <p className={styles.question}>
+          問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い
+          問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い
+        </p>
+
+        <p className={styles.answer}>あなたの考え</p>
+        <textarea className={ styles["text-area"] } />
+      </div>
+      <div className={styles["button-area"]}>
+        <LinkButton href="/complete">答える！</LinkButton>
+      </div>
     </div>
   );
 };
