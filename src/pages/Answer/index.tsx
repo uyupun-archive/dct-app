d403e5bd8b48e555a07a14a4abd7fea4375b4d43
takashi0602 <t.takashi0602@gmail.com>
2023-08-22 00:47:10 +0900

    feat: add useQueryQuestion to answer page

--- src/pages/Answer/index.tsx
+++ src/pages/Answer/index.tsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./styles.module.scss";
 import { Button } from "../../components/Button";
+import { useQueryQuestion } from "../../hooks/useQueryQuestion";
 
 const Answer: React.FC = () => {
+  const navigate = useNavigate();
+  const { data } = useQueryQuestion();
+
+  useEffect(() => {
+    if (data === null) {
+      navigate("/");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (data === null) {
+    return <></>;
+  }
+
   return (
     <form onSubmit={() => {}} className={styles.container}>
       <div>
         <h1 className={styles.heading}>まとめ</h1>
-
-        <p className={styles.question}>
-          問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い
-          問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い問い
-        </p>
-
-        <label htmlFor="answer" className={styles.answer}>
-          あなたの考え
-        </label>
+        <p className={styles.question}>Q. {data.question}</p>
+        <div className={styles["label-area"]}>
+          <label htmlFor="answer" className={styles.label}>
+            あなたの考え
+          </label>
+        </div>
         <textarea id="answer" name="answer" className={styles["text-area"]} />
       </div>
-      <div className={styles["button-area"]}>
+      <div>
         <Button type="submit">答える！</Button>
       </div>
     </form>
